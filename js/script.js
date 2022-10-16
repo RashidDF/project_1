dbfc6c3012ea50b4a2d1e0eb3cc3fa6eadfc413f
Rashid Fattakhov <fattakhov.rachid@gmail.com>
2022-10-16 18:25:57 -0400

    Changement script

--- js/script.js
+++ js/script.js
@@ -9,12 +9,30 @@ const personMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько его оцениваете?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько его оцениваете?', '');
+// for(let i=0; i<2; i++){
+//     const 
+//     a = prompt('Один из последних просмотренных фильмов?', ''),
+//     b = prompt('На сколько его оцениваете?', '');
+    
+//     if(a != null && b != null && a != '' && b != '' && a.length < 50){
+//         personMovieDB.movies[a] = b;
+//     } else {
+//         console.log('Error');
+//         i--;
+//     }         
+// }
 
-personMovieDB.movies[a] = b;
-personMovieDB.movies[c] = d;
+if(personMovieDB.count > 0 && personMovieDB.count < 10){
+    console.log('Просмотренно довольно мало фильмов');
+} else if(personMovieDB.count >= 10 && personMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else if(personMovieDB.count == null || personMovieDB.count == ''){
+    console.log('Error');
+} else {
+    console.log('Error');
+}
 
-console.log(personMovieDB);
+// console.log(personMovieDB);
+console.log(personMovieDB.count);
